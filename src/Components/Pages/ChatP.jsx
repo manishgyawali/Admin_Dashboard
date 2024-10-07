49a77fe0606fb359790cc17f76e45b70ae68d23c
manishgyawali <manishgyawali345@gmail.com>
2024-10-07 20:55:09 +0545

    chat added

--- src/Components/Pages/ChatP.jsx
+++ src/Components/Pages/ChatP.jsx
@@ -85,21 +85,55 @@ const ChatP = () => {
           </div>
         </div>
 
-        <div className="bg-white p-5 flex flex-col gap-5  rounded-md ">
-          <div>
+        <div className="bg-white px-5 pt-8 flex flex-col gap-5  rounded-md ">
+          
             <div className="flex gap-3 items-center">
-            <img
-                  src="https://plus.unsplash.com/premium_photo-1689539137236-b68e436248de?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  alt=""
-                  className="w-10 h-10 object-cover "
-                />
-                <div className="grid gap-2 items-center">
-                    <h1>Travor James</h1>
-                    <h1>Yesterday at 6.33 pm</h1>
-                </div>
+              <img
+                src="https://plus.unsplash.com/premium_photo-1689539137236-b68e436248de?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                className="w-10 h-10 object-cover "
+              />
+              <div className="grid gap-1 items-center">
+                <h1>Travor James</h1>
+                <h1 className="text-gray-400">Yesterday at 6.33 pm</h1>
+              </div>
             </div>
-          </div>
+            <div className="bg-[#4C6EF8] text-sm rounded-md text-white p-8 flex flex-col gap-5">
+              <div className="flex flex-col gap-3">
+                <h1>Dear KK,</h1>
+                <h1>Thank you for your update.</h1>
+              </div>
+              <p>We do not sell or share your details without your permission. Find out more in our Privacy Policy. Your username, email and password can be updated via your Codepixar Account settings.</p>
+            <h1>Regards,</h1> 
+            </div>
+            <div className=" gap-3  flex justify-end items-center space-x-4">
+             
+              <div className="grid gap-1 items-center text-right">
+                <h1>Travor James</h1>
+                <h1 className="text-gray-400">Yesterday at 6.33 pm</h1>
+              </div>
+              <img
+                src="https://plus.unsplash.com/premium_photo-1689539137236-b68e436248de?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                className="w-10 h-10 object-cover "
+              />
+            </div>
+            <div className="bg-[#F5F6FF] text-sm rounded-md text-gray-500 p-8 flex flex-col gap-5">
+              <div className="flex flex-col gap-3">
+                <h1>Dear KK,</h1>
+                <h1>Thank you for your update.</h1>
+              </div>
+              <p>We do not sell or share your details without your permission. Find out more in our Privacy Policy. Your username, email and password can be updated via your Codepixar Account settings.</p>
+            <h1>Regards,</h1>
+            </div>
+            <div className="flex justify-between items-center pt-6">
+              <input type="text" placeholder="Write your message" className=" border-2 py-2 rounded-md placeholder:text-sm  px-5" />
+              <button className="bg-[#4C6EF8] px-10 py-2 rounded-md text-white">Send</button>
+            </div>
+        
         </div>
+
+        
       </div>
     </div>
   );
