b8db06c8affa9aae9517f25dcb918fd5eb56c20f
manishgyawali <manishgyawali345@gmail.com>
2024-10-30 10:47:10 +0545

    cart checkout added

--- src/Components/Pages/Products/CartP.jsx
+++ src/Components/Pages/Products/CartP.jsx
@@ -1,9 +1,154 @@
-import React from 'react'
+import React from "react";
+import { PiGreaterThan } from "react-icons/pi";
+import { AiOutlineClose } from "react-icons/ai";
+import Laptop from "../../../assets/Image/laptop.png";
+import Glasses from "../../../assets/Image/glasses.png";
+import Headphone from "../../../assets/Image/headphone.png";
 
 const CartP = () => {
   return (
-    <div>CartP</div>
-  )
-}
+    <div className=" bg-[#EDF1F3] ">
+      <div className="bg-[#64C5B1] py-5">
+        <div className="flex justify-between items-start w-11/12 mx-auto">
+          <div className="flex flex-col gap-3">
+            <h1 className="text-2xl text-white font-semibold">Products</h1>
+            <h3 className="text-white text-sm flex items-center gap-2">
+              Salessa <PiGreaterThan className="text-sm" /> Dashboard{" "}
+              <PiGreaterThan className="text-sm" /> Products
+            </h3>
+          </div>
+          <button className="px-5 text-sm py-2 rounded shadow-2xl mt-5 text-black bg-white hover:bg-blue-500">
+            Create Report
+          </button>
+        </div>
+      </div>
 
-export default CartP
+      <div className="w-11/12 mx-auto mb-14 ">
+        <table className="w-full text-left bg-white rounded-md">
+          <thead>
+            <tr className="bg-[#F3F9FB] text-[#64C5B1] text-sm font-semibold">
+              <th className="py-4 px-6">Product</th>
+              <th className="py-4 px-6">Price</th>
+              <th className="py-4 px-6">Quantity</th>
+              <th className="py-4 px-6">Total</th>
+              <th className="py-4 px-6">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="border-t">
+              <td className="py-4 px-6 flex items-center gap-3">
+                <img src={Laptop} alt="Reebok Bag" className="w-10 h-10" />
+                <div>
+                  <p className="font-semibold text-black">Laptop</p>
+                  <p className="text-sm text-gray-400">size-08 (Model 2019)</p>
+                </div>
+              </td>
+              <td className="py-4 px-6 text-black">$99</td>
+              <td className="py-4 px-6">
+                <input
+                  type="number"
+                  defaultValue="2"
+                  className="border p-1 w-12 text-center"
+                />
+              </td>
+              <td className="py-4 px-6 text-black">$198</td>
+              <td className="py-4 px-6 text-center">
+                <AiOutlineClose className="text-gray-500 cursor-pointer" />
+              </td>
+            </tr>
+            <tr className="border-t">
+              <td className="py-4 px-6 flex items-center gap-3">
+                <img src={Glasses} alt="White Shoes" className="w-10 h-10" />
+                <div>
+                  <p className="font-semibold text-black">Glasses</p>
+                  <p className="text-sm text-gray-400">size-06 (Model 2019)</p>
+                </div>
+              </td>
+              <td className="py-4 px-6 text-black">$75</td>
+              <td className="py-4 px-6">
+                <input
+                  type="number"
+                  defaultValue="2"
+                  className="border p-1 w-12 text-center"
+                />
+              </td>
+              <td className="py-4 px-6 text-black">$150</td>
+              <td className="py-4 px-6 text-center">
+                <AiOutlineClose className="text-gray-500 cursor-pointer" />
+              </td>
+            </tr>
+            <tr className="border-t">
+              <td className="py-4 px-3 flex items-center gap-3">
+                <img src={Headphone} alt="Lava Watch" className="w-14 h-15" />
+                <div>
+                  <p className="font-semibold text-black">Lava Headphone</p>
+                  <p className="text-sm text-gray-400">
+                    Pure Leather 100% (Model 2019)
+                  </p>
+                </div>
+              </td>
+              <td className="py-4 px-6 text-black">$49</td>
+              <td className="py-4 px-6">
+                <input
+                  type="number"
+                  defaultValue="1"
+                  className="border p-1 w-12 text-center"
+                />
+              </td>
+              <td className="py-4 px-6 text-black">$49</td>
+              <td className="py-4 px-6 text-center">
+                <AiOutlineClose className="text-gray-500 cursor-pointer" />
+              </td>
+            </tr>
+            <tr className="border-t">
+              <td className="py-4 px-6 flex items-center gap-3">
+                <img
+                  src={Glasses}
+                  alt="Important Shoes"
+                  className="w-10 h-10"
+                />
+                <div>
+                  <p className="font-semibold text-black">Important Glasses</p>
+                  <p className="text-sm text-gray-400">size-07 (Model 2019)</p>
+                </div>
+              </td>
+              <td className="py-4 px-6 text-black">$99</td>
+              <td className="py-4 px-6">
+                <input
+                  type="number"
+                  defaultValue="1"
+                  className="border p-1 w-12 text-center"
+                />
+              </td>
+              <td className="py-4 px-6 text-black">$99</td>
+              <td className="py-4 px-6 text-center">
+                <AiOutlineClose className="text-gray-500 cursor-pointer" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="bg-white pt-10 px-10  grid justify-end gap-7 ">
+          <h1 className="text-xl">Total Payment</h1>
+          <div className="flex gap-5 justify-end">
+            <div className="flex flex-col gap-10 text-sm text-gray-800">
+              <h1>Subtotal</h1>
+              <h1>Shipping</h1>
+              <h1>Promo Code</h1>
+              <h1>Total</h1>
+            </div>
+            <div className="flex flex-col gap-10  text-sm text-gray-800">
+              <h1>$496.00</h1>
+
+              <h1>Shipping Charge : $5.00</h1>
+
+              <h1>-$10.00</h1>
+              <h1 className="text-black font-semibold"> $491.00</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartP;
--- src/Components/Pages/Products/CheckoutP.jsx
+++ src/Components/Pages/Products/CheckoutP.jsx
@@ -1,9 +1,204 @@
-import React from 'react'
+import React from "react";
+import { PiGreaterThan } from "react-icons/pi";
 
+import Laptop from "../../../assets/Image/laptop.png";
+import Glasses from "../../../assets/Image/glasses.png";
+import Headphone from "../../../assets/Image/headphone.png";
+import Mastercard from "../../../assets/Image/mastercard.png";
+import Paypal from "../../../assets/Image/paypal.png";
 const CheckoutP = () => {
   return (
-    <div>CheckoutP</div>
-  )
-}
+    <div className=" bg-[#EDF1F3] ">
+      <div className="bg-[#64C5B1] py-5">
+        <div className="flex justify-between items-start w-11/12 mx-auto">
+          <div className="flex flex-col gap-3">
+            <h1 className="text-2xl text-white font-semibold">Checkout</h1>
+            <h3 className="text-white text-sm flex items-center gap-2">
+              Salessa <PiGreaterThan className="text-sm" /> Dashboard{" "}
+              <PiGreaterThan className="text-sm" /> Checkout
+            </h3>
+          </div>
+          <button className="px-5 text-sm py-2 rounded shadow-2xl mt-5 text-black bg-white hover:bg-blue-500">
+            Create Report
+          </button>
+        </div>
+      </div>
 
-export default CheckoutP
+      <div className="grid grid-cols-3 gap-5 m-5 rounded-md">
+        <div className="bg-white shadow-lg rounded-md w-80 p-5">
+          <h2 className="font-semibold text-[#2E2E5F] mb-4 text-2xl  pl-4 pb-5">
+            Order Summary
+          </h2>
+          <table className="w-full text-left">
+            <thead>
+              <tr className="border-b ">
+                <th className="pb-2 pl-10 font-medium">Product</th>
+                <th className="pb-2  font-medium">Quantity</th>
+                <th className="pb-2  font-medium">Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="border-b py-4">
+                <td className="py-3 flex flex-col items-center gap-2">
+                  <img src={Laptop} alt="Reebok Bag" className="w-12 h-12" />
+                  <p className="text-sm font-medium text-black text-center">
+                    Reebok Shoes
+                  </p>
+                </td>
+                <td className="text-center py-3">2</td>
+                <td className="text-center py-3">$198</td>
+              </tr>
+              <tr className="border-b py-4">
+                <td className="py-3 flex flex-col items-center gap-2">
+                  <img src={Glasses} alt="White Camera" className="w-12 h-12" />
+                  <p className="text-sm font-medium text-black text-center">
+                    White Modern Camera
+                  </p>
+                </td>
+                <td className="text-center py-3">2</td>
+                <td className="text-center py-3">$150</td>
+              </tr>
+              <tr className="border-b py-4">
+                <td className="py-3 flex flex-col items-center gap-2">
+                  <img src={Headphone} alt="White Camera" className=" h-12" />
+                  <p className="text-sm font-medium text-black text-center">
+                    White Modern Camera
+                  </p>
+                </td>
+                <td className="text-center py-3">2</td>
+                <td className="text-center py-3">$150</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className="bg-white shadow-lg rounded-md w-80 p-5">
+          <h2 className="font-semibold text-[#2E2E5F] mb-4 text-2xl   pl-4 pb-5">
+            Delivery Address
+          </h2>
+
+          <div className="flex flex-col gap-7">
+            <div className="flex gap-5">
+              <label htmlFor="" className="w-12">
+                First Name
+              </label>
+              <input
+                type="text"
+                className="border border-gray-500  rounded-md "
+              />
+            </div>
+            <div className="flex gap-5">
+              <label htmlFor="" className="w-12">
+                Last Name
+              </label>
+              <input
+                type="text"
+                className="border border-gray-500 py-1  rounded-md "
+              />
+            </div>
+            <div className="flex gap-5">
+              <label htmlFor="" className="w-12">
+                Delivery Address
+              </label>
+              <input
+                type="text"
+                className="border border-gray-500 py-1  rounded-md "
+              />
+            </div>{" "}
+            <div className="flex gap-5">
+              <label htmlFor="" className="w-12">
+                City
+              </label>
+              <input
+                type="text"
+                className="border border-gray-500 py-1  rounded-md "
+              />
+            </div>{" "}
+            <div className="flex gap-5">
+              <label htmlFor="" className="w-12">
+                State
+              </label>
+              <input
+                type="text"
+                className="border border-gray-500 py-1  rounded-md "
+              />
+            </div>{" "}
+            <div className="flex gap-5">
+              <label htmlFor="" className="w-12">
+                Country
+              </label>
+              <input
+                type="text"
+                className="border border-gray-500 py-1  rounded-md "
+              />
+            </div>{" "}
+            <div className="flex gap-5">
+              <label htmlFor="" className="w-12">
+                Zip Code
+              </label>
+              <input
+                type="text"
+                className="border border-gray-500 py-1  rounded-md "
+              />
+            </div>{" "}
+            <div className="flex gap-5 ">
+              <label htmlFor="" className="w-12">
+                Email Address
+              </label>
+              <input
+                type="text"
+                className="border border-gray-500 py-1  rounded-md "
+              />
+            </div>
+            <div className="flex gap-5">
+              <label htmlFor="" className="w-12">
+                Mobile No
+              </label>
+              <input
+                type="text"
+                className="border border-gray-500 py-1  rounded-md "
+              />
+            </div>
+            <div className="flex gap-7">
+              <input type="checkbox" name="" id="" />
+              <h1>Confirm Shipping Address</h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-white shadow-lg rounded-md w-80 p-5">
+          <h2 className="font-semibold  text-[#2E2E5F]  mb-4 text-2xl  pl-4 pb-5">
+            Billing Details
+          </h2>
+
+          <div>
+            <div className="flex gap-5 items-center justify-evenly cursor-pointer border p-5">
+              <h1>Credit Card</h1>
+              <img src={Mastercard} alt="" className="h-10" />
+            </div>
+            <div className="grid gap-5  cursor-pointer  border p-5">
+              <h1>Card No :</h1>
+              <input type="text" placeholder="1234-5678-9123-4567" className="border-gray-400 border p-3 rounded-md" />
+              <div className="grid grid-cols-3 gap-4">
+                <div>
+                  <h1>Expiry Month</h1>
+                  <input type="month" />
+                </div>
+                <div>
+                  <h1>Expiry Month</h1>
+                  <input type="year" />
+                </div>
+                <div>
+                  <h1>Expiry Month</h1>
+                  <input type="month" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutP;
